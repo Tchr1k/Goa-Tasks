d8a51b00fdd6f9c46183fd7d4bab0121170e66e2
Nikoloz <nikoloztchriki@gmail.com>
2026-01-14 19:41:08 +0400

    main

---
+++ Day 38/Class Work/mine.py
@@ -0,0 +1,19 @@
+fruits = ("Apple","Banana","Orange","Goldan Apple","Mango")
+
+fruits1,fruits2,fruits3,fruits4,fruits5 = fruits 
+
+print(fruits1,fruits2,fruits3,fruits4,fruits5)
+
+# tuple არის მრავალი ელემენტების კრებული
+# tuple იწერება მრგვალი ფრჩხილებით ()
+
+# list-ის გან განსხვავებით Tuple-ში არ შეგვიძლია ამოშლა,დამატება,შეცვლა.
+
+
+name = ("Nika","Tsotne","Sandro","Deme","Nika","Ibrogimovich")
+
+print(name.count("Nika"))
+print(name.index("Tsotne"))
+
+
+# rest-ი აღნიშნავს unpucking-ის დროს კონკრეტული ელემენტის შემდეგ ყველა ელემენტს
